36bba13d8aa44b8f15d90f3d56cd31fbf54c8d9d
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-07-08 15:13:07 +0200

    Add new file

---
+++ src/tools/ui.ts
@@ -0,0 +1,44 @@
+import type { SwapFTData, TokenInfo } from "@bitte-ai/types";
+import { getChainById } from "@bitte-ai/agent-sdk";
+import { Currency, CurrencyAmount, Token } from "@uniswap/sdk-core";
+
+interface SwapDetails {
+  chainId: number;
+  // route: SwapRoute;
+  input: CurrencyAmount<Currency>;
+  output: CurrencyAmount<Currency>;
+}
+
+export function parseWidgetData({
+  chainId,
+  // TODO: There is a lot more data that can be parsed out of the route for UI purposes!
+  // route,
+  input,
+  output,
+}: SwapDetails): SwapFTData {
+  const chain = getChainById(chainId);
+
+  return {
+    network: {
+      name: chain.name,
+      icon: "",
+    },
+    type: "swap",
+    fee: "0",
+    tokenIn: basicCurrencyInfo(input),
+    tokenOut: basicCurrencyInfo(output),
+  };
+}
+
+function basicCurrencyInfo(amount: CurrencyAmount<Currency>): TokenInfo {
+  // TODO (bh2smith): wont work for native assets.
+  const token = amount.currency as Token;
+  return {
+    contractAddress: token.address,
+    amount: amount.toExact(),
+    usdValue: 0,
+    name: token.name || token.symbol!,
+    symbol: token.symbol!,
+    decimals: token.decimals,
+  };
+}
